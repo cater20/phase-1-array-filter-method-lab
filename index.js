0f30c778fe7642f226005b196c51f595d0df681f
cater20 <onyangoketta@gmail>
2022-06-12 13:42:45 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+
+
+//function findMatching(drivers,string){
+
+//{
+    //return drivers.filter((driver)=>drivers.toLowerCase()=== string.toLowerCase());
+//}
+//}
+//const fuzzyMatch=(drivers,string)=>{
+//    return drivers.filter((driver)) => drivers.chartAt(0)===string.chartAt(0)
+//}
+const findMatching =(names, returnDrivers)=>{
+    return names.filter((filterdNames) => 
+    filterdNames.toLowerCase() === returnDrivers.toLowerCase());
+}
+
+const fuzzyMatch = (names, returnDrivers) =>{
+    return names.filter((filterdNames) => 
+    filterdNames.toLowerCase().indexOf(returnDrivers.toLowerCase()) === 0);
+}
+
+const matchName = (names, hometown) =>{
+    return names.filter((filterdNames) => 
+    filterdNames.name ===hometown)
+}
